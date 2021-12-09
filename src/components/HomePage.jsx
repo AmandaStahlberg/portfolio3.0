e69ddbbd40df46bacbc2c73229ada1d371df2b52
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2021-12-09 18:13:40 +0100

    add animation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,27 @@
 import '../styles/homepage.css';
+import {
+  SlidingFromLeftAnimation,
+  SlidingFromRightAnimation,
+} from './animations';
 //import Amanda from '../assets/Amanda.svg'
 //import Amanda from '../assets/Component1.svg'
-
-
 
 const HomePage = () => {
   return (
     <div className="Home">
       <div>
-        <h1>AMAND</h1>
-        <h1>ASTÅH</h1>
-        <h1>LBERG</h1>
+        <h1></h1>
+        <h1>AS</h1>
+        <h1></h1>
+        <SlidingFromLeftAnimation>
+          <h1>AMAND</h1>
+        </SlidingFromLeftAnimation>
+        <SlidingFromRightAnimation>
+          <h1>ASTÅH</h1>
+        </SlidingFromRightAnimation>
+        <SlidingFromLeftAnimation>
+          <h1>LBERG</h1>
+        </SlidingFromLeftAnimation>
       </div>
     </div>
   );
---
+++ src/components/animations.jsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { motion } from 'framer-motion'
+
+const nameVariants = {
+    hidden: {
+        opacity: 0,
+        x: "-130vw",
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 3,
+        }
+    }
+}
+
+const SlidingFromRightVariants = {
+    hidden: {
+        opacity: 0,
+        x: "130vw",
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 3,
+        }
+    }
+}
+
+// const SpinningTextVariants = {
+//     start: {
+//         scale: 0
+//     },
+//     animatee: {
+//         rotate: 360,
+//         scale: 1,
+//         transition: {
+//             type: "spring",
+//             stiffness: 240,
+//             damping: 80
+//         }
+//     }
+// }
+
+// const SpinningTextAnimation = (props) => {
+//     return (
+//         <motion.div
+//             className="size50 center"
+//             variants={SpinningTextVariants}
+//             initial="start"
+//             animate="animatee"
+//         >
+//             {props.children}
+//         </motion.div>
+//     )
+// }
+// export default SpinningTextAnimation;
+
+
+
+export const SlidingFromLeftAnimation = (props) => {
+    return (
+        <motion.div 
+        className="size50 center"
+        variants={nameVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        >
+            {props.children}
+        </motion.div>
+    )
+}
+
+
+
+
+
+export const SlidingFromRightAnimation = (props) => {
+    return (
+        <motion.div 
+        variants={SlidingFromRightVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        >
+            {props.children}
+        </motion.div>
+    )
+}
+
